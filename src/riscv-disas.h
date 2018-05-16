633e81e888066d3081a780b5ec302d5db1081453
Michael Clark <michaeljclark@mac.com>
2018-05-16 21:51:59 +1200

    Update copyright date

--- src/riscv-disas.h
+++ src/riscv-disas.h
@@ -2,7 +2,7 @@
  * RISC-V Disassembler
  *
  * Copyright (c) 2016-2017 Michael Clark <michaeljclark@mac.com>
- * Copyright (c) 2017 SiFive, Inc.
+ * Copyright (c) 2017-2018 SiFive, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
