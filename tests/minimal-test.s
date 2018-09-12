3b6606c6946251325e391c729302fa74bcfc081a
Michael Clark <mjc@sifive.com>
2018-09-12 17:57:01 +1200

    update tests to note c.addi r,r,0 won't assemble in gas

--- tests/minimal-test.s
+++ tests/minimal-test.s
@@ -4,9 +4,9 @@ _start:
 .half 0         # unimp
 .half 1         # c.nop
 .half 0xd       # c.nop 3
-.half 0x401     # illegal (c.addi 0)
-addi          s1,sp,512
+.half 0x401     # add s0,s0,0 (gas disassembles)
 addi          s0,s0,1
+addi          s1,sp,512
 csrrs         a0,mhartid,zero
 auipc         a1,0
 lui           t0,132096
