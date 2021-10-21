914aa7304b90da2d628d889a49767c0cc002780e
Michael Clark <michaeljclark@mac.com>
2021-10-22 11:41:59 +1300

    add disassemble function that uses the inst_fetch function

--- src/test-decode.c
+++ src/test-decode.c
@@ -2,32 +2,40 @@
 
 #include "riscv-disas.h"
 
-void print_insn(int64_t pc, int64_t inst)
+#define array_size(arr) (sizeof(arr) / sizeof(arr[0]))
+
+void dissassemble(uint64_t pc, const uint8_t *data, size_t data_len)
 {
     char buf[128] = { 0 };
-    disasm_inst(buf, sizeof(buf), rv64, pc, inst);
-    printf("0x%" PRIx64 ":  %s\n", pc, buf);
+    size_t offset = 0, inst_len;
+    rv_inst inst;
+    while (offset < data_len)
+    {
+        inst_fetch(data + offset, &inst, &inst_len);
+        disasm_inst(buf, sizeof(buf), rv64, pc + offset, inst);
+        printf("0x%" PRIx64 ":  %s\n", pc + offset, buf);
+        offset += inst_len;
+    }
 }
 
-static uint64_t inst_arr[] = {
-    0x0,
-    0x1,
-    0xd,
-    0x401,
-    0x404,
-    0x405,
-    0xf1402573,
-    0x597,
-    0x204002b7,
-    0x13,
-};
+void t1()
+{
+    static const uint8_t inst_arr[] = {
+        0x00, 0x00, // illegal
+        0x00, 0x01, // nop
+        0x00, 0x0d, // addi zero,zero,3
+        0x01, 0x04, // mv s0,s0
+        0x04, 0x04, // addi s1,sp,512
+        0x05, 0x40, // addi  s0,s0,1
+        0x73, 0x25, 0x40, 0xf1, // csrrs a0,mhartid,zero
+        0x97, 0x05, 0x00, 0x00, // auipc a1,0
+        0xb7, 0x02, 0x40, 0x20, // lui t0,541065216
+        0x13, 0x00, 0x00, 0x00, // nop
+    };
+    dissassemble(0x10078, inst_arr, array_size(inst_arr));
+}
 
 int main()
 {
-	uint64_t pc = 0x10078;
-	for (size_t i = 0; i < sizeof(inst_arr) / sizeof(inst_arr[0]); i++) {
-		uint64_t inst = inst_arr[i];
-		print_insn(pc, inst);
-		pc += inst_length(inst);
-	}
+    t1();
 }
